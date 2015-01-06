c0b2290d78107a46766e29fbed4522bc973fedf8
Andrey Khozov <avkhozov@gmail.com>
2015-01-06 20:41:06 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Minion::Backend::Mango - It's new $module
+
+# SYNOPSIS
+
+    use Minion::Backend::Mango;
+
+# DESCRIPTION
+
+Minion::Backend::Mango is ...
+
+# LICENSE
+
+Copyright (C) Andrey Khozov.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Andrey Khozov <avkhozov@gmail.com>
---
+++ lib/Minion/Backend/Mango.pm
@@ -0,0 +1,39 @@
+package Minion::Backend::Mango;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Minion::Backend::Mango - It's new $module
+
+=head1 SYNOPSIS
+
+    use Minion::Backend::Mango;
+
+=head1 DESCRIPTION
+
+Minion::Backend::Mango is ...
+
+=head1 LICENSE
+
+Copyright (C) Andrey Khozov.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Andrey Khozov E<lt>avkhozov@gmail.comE<gt>
+
+=cut
+
